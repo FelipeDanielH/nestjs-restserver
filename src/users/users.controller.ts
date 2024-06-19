c8f26002bf7b3e5857edbbde29ffb2222b98e1bf
Felipe <felipe.daniel.henriquez@gmail.com>
2024-06-18 20:43:10 -0400

    feat(users): agregado http estatus

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Param, ParseIntPipe, Delete, Patch } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, ParseIntPipe, Delete, Patch, HttpException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
@@ -15,17 +15,17 @@ export class UsersController {
     }
 
     @Get(':id')
-    getUser(@Param('id', ParseIntPipe) id: number ): Promise<User> {
+    getUser(@Param('id', ParseIntPipe) id: number): Promise<User | HttpException> {
         return this.usersService.getUser(id);
     }
 
     @Post()
-    createUser(@Body() newUser:CreateUserDto ): Promise<User> {
+    createUser(@Body() newUser: CreateUserDto): Promise<User | HttpException> {
         return this.usersService.createUser(newUser);
     }
 
     @Delete(':id')
-    deleteUser(@Param('id', ParseIntPipe ) id: number){
+    deleteUser(@Param('id', ParseIntPipe) id: number) {
         return this.usersService.deleteUser(id);
     }
 
@@ -33,7 +33,7 @@ export class UsersController {
     updateUser(
         @Param('id', ParseIntPipe) id: number,
         @Body() user: UpdateUserDto
-    ){
+    ) {
         return this.usersService.updateUser(id, user)
     }
 
